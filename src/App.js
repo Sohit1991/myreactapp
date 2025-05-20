056c95a21469d78b93bbc0ff8fab590f974661d4
Sohit <sohitkhanchi@gmail.com>
2025-05-20 22:57:17 +0530

    initialise project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+        </p>
+        <p> * Veg Berger :  49</p> 
+        <p> * Aloo Berger : 69</p>
+        <p> * Straw Berry Shakes : 89</p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React yyyy
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
